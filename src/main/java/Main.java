74570faaa4ca870306cf2f1793c3e8aaa5364d55
Mohammed <mohammed@Zipcoders-MacBook-Pro-9.local>
2024-08-14 19:07:32 -0400

    project complete and functional, needs to be optimized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
 import org.apache.commons.io.IOUtils;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     String result;
-    ArrayList<String> lists = new ArrayList<>();
+    ArrayList<String> arraylists = new ArrayList<>();
+    int errors=0;
+    LinkedHashMap<String, Integer> list = new LinkedHashMap<>();
+
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
@@ -25,35 +31,55 @@ public class Main {
             String match = matcher.group();
             list.add(match);
         }
-        this.lists = list;
+        this.arraylists = list;
         return list;
     }
 
-    public HashMap<String, Integer> createItemHashMap(ArrayList<String> lists, String item) {
-        HashMap<String, Integer> list = new HashMap<>();
-        Pattern pattern = Pattern.compile(item, Pattern.CASE_INSENSITIVE);
+    public LinkedHashMap<String, Integer> createHashMapForItem(ArrayList<String> lists, String regexItem) {
+       // HashMap<String, Integer> list = new HashMap<>();
+       checkIfWordIsMisSpelled(regexItem);
+        Pattern pattern = Pattern.compile(regexItem, Pattern.CASE_INSENSITIVE);
+
         for( int i =0; i< lists.size(); i++){
+
             Matcher matcher = pattern.matcher(lists.get(i));
+
             if(matcher.find()){
-                System.out.println(lists.get(i));
+             //   updateItemHashMap(capitalizeFirstLetter(matcher.group()), value);
+                String item = capitalizeFirstLetter(matcher.group());
                 String price = getPriceOfItem(lists.get(i));
-                System.out.println("price is: " + price);
-                // check if key is in the lists, if it is, increase value by one, if not add the key and value to the list
-                list.put(price, list.getOrDefault(price, 0)+1);
-//                if(list.containsKey(price)){
-//                    list.set
-//                }
+                if(price != null){
+                    list.put(item,list.getOrDefault(item, 0)+1);
+                    list.put(price, list.getOrDefault(price, 0)+1);
+                }
             }
-
-                    for (Map.Entry<String, Integer> entry : list.entrySet()) {
-                        String key = entry.getKey();
-                        Integer value = entry.getValue();
-                        System.out.println("Key=" + key + ", Value=" + value);
-                    }
-
-
         }
+        for (Map.Entry<String, Integer> entry : list.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("Key=" + key + ", Value=" + value);
+        }
+
         return list;
+    }
+
+    private void checkIfWordIsMisSpelled(String regexItem) {
+        list.clear();
+        if(regexItem.equals("\\bcookies\\b")) {
+           // regexItem = "(?i)(C......)";
+            Pattern pattern = Pattern.compile("Co0kies", Pattern.CASE_INSENSITIVE);
+            for (int i = 0; i < this.arraylists.size(); i++) {
+                Matcher matcher = pattern.matcher(arraylists.get(i));
+                if(matcher.find()){
+                    list.put("Cookies", 1);
+                    list.put(getPriceOfItem(arraylists.get(i)), 1);
+                }
+            }
+        }
+    }
+
+    public String capitalizeFirstLetter(String string) {
+      return string.substring(0,1).toUpperCase() + string.substring(1).toLowerCase();
     }
 
     private String getPriceOfItem(String s) {
@@ -63,9 +89,67 @@ public class Main {
         if(matcher.find()){
             return matcher.group();
         }
-
-        return "-1";
+        return null;
     }
 
+    public String findErrors(String typeOfError, String string){
+        Pattern pattern = Pattern.compile(typeOfError, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(string);
+        while(matcher.find()){
+           this.errors++;    // :;
+        }
+        return "Errors         	 	 seen: " +this.errors +  " times";
+    };
+
+
+    public void writeFile(String results) {
+        try {
+         //  String results = toStringHashMap(hash);
+            FileWriter fileWriter = new FileWriter("src/main/resources/outPut2.txt", true);
+            System.out.println("file created");
+            fileWriter.write(results);
+            System.out.println("succesfully wrote to file");
+           fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        String results ="";
+        String string = main.readRawDataToString();
+        ArrayList<String> lists= main.splitStringintoAnArrayOfElements(string);
+        LinkedHashMap<String, Integer> hashMilk = main.createHashMapForItem(lists, "\\bmilk\\b");
+        results += main.toStringHashMap(hashMilk);
+        LinkedHashMap<String, Integer> hashBread = main.createHashMapForItem(lists, "\\bBread\\b");
+        results += main.toStringHashMap(hashBread);
+        LinkedHashMap<String, Integer> hashCookies = main.createHashMapForItem(lists, "\\bcookies\\b");
+        results += main.toStringHashMap(hashCookies);
+        LinkedHashMap<String, Integer> hashApples = main.createHashMapForItem(lists, "\\bapples\\b");
+        results += main.toStringHashMap(hashApples);
+        results+= main.findErrors(":;", string);
+        main.writeFile(results);
+
+    }
+
+    private String toStringHashMap(LinkedHashMap<String, Integer> hash) {
+        String results = "";
+        int i = 0;
+        for(Map.Entry<String, Integer> entry : hash.entrySet()){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            if(i == 0){
+                results += " name: " + key +"          seen: " + value + " times." +" \n ";
+                results += "============       ========" + "\n";
+                i++;
+            }else{
+                results += " price: " + key +"        seen: " + value + " times." +" \n ";
+                results += "------------       --------" + "\n";
+            }
+        }
+        results += "\n \n";
+        return results;
+    }
 }
 
--- src/main/java/TestHurtLocker.java
+++ src/main/java/TestHurtLocker.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class TestHurtLocker {
 
@@ -21,6 +22,61 @@ public class TestHurtLocker {
         String results = main.readRawDataToString();
        ArrayList<String> list =  main.splitStringintoAnArrayOfElements(results);
        String milkItem = "\\bmilk\\b";
-      HashMap<String, Integer> MilkHash = main.createItemHashMap(list, milkItem);
+      HashMap<String, Integer> MilkHash = main.createHashMapForItem(list, milkItem);
+        int actual = MilkHash.get("Milk");
+        Assert.assertEquals(6, actual);
     }
+
+    @Test
+    public void testThereAre6Breads() throws Exception {
+        Main main = new Main();
+        String results = main.readRawDataToString();
+        ArrayList<String> list =  main.splitStringintoAnArrayOfElements(results);
+        String milkItem = "\\bBread\\b";
+        LinkedHashMap<String, Integer> breadHash = main.createHashMapForItem(list, milkItem);
+        int actual = breadHash.get("Bread");
+        Assert.assertEquals(6, actual);
+    }
+
+    @Test
+    public void testThereAre4Apples() throws Exception {
+        Main main = new Main();
+        String results = main.readRawDataToString();
+        ArrayList<String> list =  main.splitStringintoAnArrayOfElements(results);
+        String milkItem = "\\bapples\\b";
+        HashMap<String, Integer> appleHash = main.createHashMapForItem(list, milkItem);
+        int actual = appleHash.get("Apples");
+        Assert.assertEquals(4, actual);
+    }
+
+    @Test
+    public void testThereAre8Cookies() throws Exception {
+        Main main = new Main();
+        String results = main.readRawDataToString();
+        ArrayList<String> list =  main.splitStringintoAnArrayOfElements(results);
+        String cookieItem = "\\bcookies\\b";
+        HashMap<String, Integer> cookieHash = main.createHashMapForItem(list, cookieItem);
+        int actual = cookieHash.get("Cookies");
+        Assert.assertEquals(8, actual);
+    }
+
+
+
+    @Test
+    public void testErrors() throws Exception {
+        Main main = new Main();
+        String results = main.readRawDataToString();
+        main.findErrors(":;", results);
+        int expected = 4;
+        Assert.assertEquals(main.errors, expected);
+    }
+
+
+
+
+
+
+
+
+
 }
